df1d8c6000a0555a70876864c50e6004130a7c22
Tiago Rodrigues <tpr09756@gmail.com>
2023-04-10 16:46:21 +0000

    expandable search bar

---
+++ SearchBar/script.js
@@ -0,0 +1,12 @@
+const searchBtn = document.getElementById("search-btn");
+
+const searchInput = document.querySelector(".search-input");
+//console.log(searchBtn, searchInput);
+searchBtn.addEventListener("click", function () {
+  // searchInput.classList.add("active-search");
+  searchInput.classList.toggle("active-search");
+  console.log(searchInput);
+});
+document.querySelector("main").addEventListener("click", () => {
+  searchInput.classList.remove("active-search");
+});
