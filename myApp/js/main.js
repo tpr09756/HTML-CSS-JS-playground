41675255160bbfd19b42472f62cc21e77734b5fb
Tiago Rodrigues <tpr09756@gmail.com>
2023-04-27 13:45:54 +0000

    Tabs Menu

--- myApp/js/main.js
+++
@@ -1,8 +0,0 @@
-require(['router'], function(router){
-        $(document).ready(function(){
-            console.log('DOM is mounted and ready');
-
-            router.start();
-        })   
-
-})
